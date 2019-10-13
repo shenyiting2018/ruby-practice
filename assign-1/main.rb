9f8b332d55e65e0125535d4f3da122aece9b6aa9
= <shenyiting2018@gmail.com>
2019-10-13 14:40:48 -0700

    add solution for q6

--- assign-1/main.rb
+++ assign-1/main.rb
@@ -1,10 +1,31 @@
 #!/usr/bin/ruby
 
 require_relative('conversation_printer.rb')
+require_relative('html_filter.rb')
 
-puts 'hello world'
+# Question 6
+puts "=======================Question 6==========================="
+template = %{
+<%= simple_form_for @project do |f| %>   
+    <%= f.input :name %>  
+    <%= f.input :description %>  
+    <h3>Tasks</h3>  
+    <div id='tasks'>     
+        <%= f.simple_fields_for :tasksdo |task| %>      
+            <%= render 'task_fields', :f => task %>    
+        <% end %>
+        <div class='links'>
+            <%= link_to_add_association 'add task', f, :tasks %>
+        </div>
+    </div>
+<%= f.submit 'Save' %>
+<% end %>
+}
 
+filter = HTMLFilter.new(template)
+filter.filter
 
+puts "======================Question 7================================="
 # Question 7
 printer = ConversationPrinter.new('ADVISOR', 'conversation.txt')
 printer.print
